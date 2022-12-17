6e614943b5c596f47c5b78643600efd456590912
ysosrss <najib72645@gmail.com>
2022-12-17 17:11:25 +0800

    Add prototype and constructor

--- script.js
+++ script.js
@@ -8,12 +8,13 @@ function Book(title, author, pages, read) {
 }
 
 Book.prototype.showInfo = function() {
-    return (title + " by " + author + ", " + pages + " pages, " + read)
+    return (this.title + " by " + this.author + ", " + this.pages + " pages, " + this.read)
 }
 
 function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read)
+    myLibrary.push(book)
 } 
 
 addBookToLibrary("The Hobbit", "Author", "356", "yes")
-console.log(myLibrary)
+console.log(myLibrary[0].showInfo)
