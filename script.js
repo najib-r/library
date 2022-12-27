5f5461e62d33f28e60abb8e08815eacfd120f71c
ysosrss <najib72645@gmail.com>
2022-12-27 17:31:50 +0800

    Add option to add books

--- script.js
+++ script.js
@@ -18,9 +18,29 @@ function addBookToLibrary(book) {
 const modal = document.getElementById('myModal')
 const newBookBtn = document.getElementById('newBtn')
 const cancelBtn = document.getElementById('cancelBtn')
+const submitBtn = document.getElementById('submitBtn')
 
 newBookBtn.addEventListener('click', () => modal.style.display = 'block')
 cancelBtn.addEventListener('click', () => modal.style.display = 'none')
+
+submitBtn.addEventListener('click', function(event) {
+    event.preventDefault()
+});
+
+submitBtn.addEventListener('click', validateForm)
+
+function validateForm() {
+    const title = document.getElementById('b-title')
+    const author = document.getElementById('b-author')
+    const pages = document.getElementById('b-pages')
+    const read = document.getElementById('b-read')
+
+    // Validate all inputs...
+
+    const book = new Book(title.value, author.value, pages.value, read.value)
+    addBookToLibrary(book)
+    showBooks()
+}
 
 const book1 = new Book("The Hobbit", "Author", "356", "yes")
 addBookToLibrary(book1)
@@ -31,11 +51,17 @@ addBookToLibrary(book2)
 const book3 = new Book("Hunger Games II", "Author 2", "332", "no")
 addBookToLibrary(book3)
 
-const main = document.querySelector('.main')
 
-for (let i = 0; i < myLibrary.length; i++) {
-    var newElement = document.createElement('div');
-    newElement.className = "card"
-    newElement.innerHTML = myLibrary[i].showInfo()
-    main.appendChild(newElement)
+function showBooks() {
+    const main = document.querySelector('.main')
+    // clear main
+    main.innerHTML = "";
+    for (let i = 0; i < myLibrary.length; i++) {
+        var newElement = document.createElement('div');
+        newElement.className = "card";
+        newElement.innerHTML = myLibrary[i].showInfo();
+        main.appendChild(newElement);
+    }
 }
+
+showBooks()
