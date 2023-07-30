db40376524abcedc5bb6611f6c5d5b21689ae34f
najib-r <najib72645@gmail.com>
2023-07-30 13:34:36 +0800

    style: add styling

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, color) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.color = color;
 }
 
 Book.prototype.showInfo = function() {
@@ -12,16 +13,23 @@ Book.prototype.showInfo = function() {
 }
 
 Book.prototype.toggleRead = function() {
-    if (this.read === "yes") {
-        this.read = "no";
+    if (this.read === "already read") {
+        this.read = "not read";
     } else {
-        this.read = "yes";
+        this.read = "already read";
     }
 }
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
 } 
+
+function generateRandomColor() {
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    return bgColor = "rgb(" + x + "," + y + "," + z + ")";
+}
 
 const modal = document.getElementById('myModal');
 const newBookBtn = document.getElementById('newBtn');
@@ -44,12 +52,12 @@ submitBtn.addEventListener('click', function validateForm(event) {
      // Validate all inputs...
 
     if (read.checked) {
-        read = "yes";
+        read = "already read";
     } 
     else {
-        read = "no";
+        read = "not read";
     }
-    const book = new Book(title.value, author.value, pages.value, read);
+    const book = new Book(title.value, author.value, pages.value, read, generateRandomColor());
     addBookToLibrary(book);
     showBooks();
     modal.style.display = 'none';
@@ -74,6 +82,7 @@ function showBooks() {
     for (let i = 0; i < myLibrary.length; i++) {
         var newElement = document.createElement('div');
         newElement.className = "card";
+        newElement.style.backgroundColor = myLibrary[i].color;
         newElement.dataset.index = i;
         newElement.innerHTML = myLibrary[i].showInfo();
         const buttons = document.createElement('div');
